172591fda999dada49e049638d10338dfa5d69c5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-06-17 15:27:50 -0700

    added base classes

---
+++ UBR Tutorial Series/Assets/Scripts/RichMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public abstract class RichMonoBehaviour : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField]
+    [TextArea]
+    protected string developerDescription = "Enter a description.";
+#endif
+
+    /// <summary>
+    /// Cached Transform.
+    /// </summary>
+    protected Transform myTransform;
+
+    /// <summary>
+    /// Cached Transform.
+    /// </summary>
+    public new Transform transform { get => myTransform; }
+
+    protected virtual void Awake()
+    {
+        GatherReferences();
+    }
+
+    /// <summary>
+    /// Cache needed component references.
+    /// </summary>
+    protected virtual void GatherReferences()
+    {
+        myTransform = this.GetComponent<Transform>();
+    }
+}
