fb4371c9a010bbc2bb2bc1e96b8027af68d0d4ec
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-06-17 15:22:24 -0700

    removed ./Library from version control

---
+++ UBR Tutorial Series/Assets/Scripts/Editor/SceneMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class SceneMenu
+{
+    //TEMPLATE
+    //[MenuItem("Scenes/Empire's Edge Galaxy Map")]
+    //private static void LoadBoardScene()
+    //{
+    //    EditorSceneManager.OpenScene("Assets/Scenes/EmpiresEdge.unity");
+    //}
+    
+}
