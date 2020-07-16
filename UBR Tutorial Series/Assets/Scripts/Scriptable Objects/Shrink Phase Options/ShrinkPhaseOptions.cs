c35c2c37b93ac1c4d495dae2287811cb8008e8f1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-16 14:18:22 -0700

    developer-set only. externally readonly

--- UBR Tutorial Series/Assets/Scripts/Scriptable Objects/Shrink Phase Options/ShrinkPhaseOptions.cs
+++ UBR Tutorial Series/Assets/Scripts/Scriptable Objects/Shrink Phase Options/ShrinkPhaseOptions.cs
@@ -5,8 +5,15 @@ namespace PolygonPilgrimage.BattleRoyaleKit
     [CreateAssetMenu(fileName = "ShrinkPhaseOption_", menuName = "ScriptableObjects/ShrinkPhaseOption", order = 1)]
     public class ShrinkPhaseOptions : ScriptableObject
     {
-        public int startingRadius = 1000;
-        public ShrinkPhase[] shrinkPhases = new ShrinkPhase[7];
+        [SerializeField]
+        private int startingRadius = 1000;
+
+        public int StartingRadius { get => startingRadius; } // readonly
+
+        [SerializeField]
+        private ShrinkPhase[] shrinkPhases = new ShrinkPhase[7];
+
+        public ShrinkPhase[] ShrinkPhases { get => shrinkPhases; } // readonly
     }
 
 }
