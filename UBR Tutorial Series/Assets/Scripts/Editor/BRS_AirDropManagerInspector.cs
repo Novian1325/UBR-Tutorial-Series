b9387ec26fa1571a95e0b076ac900123cdf94169
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-15 22:46:17 -0700

    new Inspector for Supply Drop Manager

---
+++ UBR Tutorial Series/Assets/Scripts/Editor/BRS_AirDropManagerInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    [CustomEditor(typeof(SupplyDropManager))]
+    public class BRS_AirDropManagerInspector : Editor
+    {
+        private SupplyDropManager inspectedInstance;
+
+        private void OnEnable()
+        {
+            inspectedInstance = target as SupplyDropManager;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            //buttons 
+            GUILayout.Space(10);
+            
+            if(GUILayout.Button("Trigger Supply Drop"))
+            {
+                inspectedInstance.DeploySupplyDrop();
+            }
+        }
+
+    }
+
+}
