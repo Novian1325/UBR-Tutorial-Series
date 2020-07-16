8e6284b9804fe1c4edf347c52b76e35958d0fdce
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-16 14:18:33 -0700

    developer set only -- externally readonly

--- UBR Tutorial Series/Assets/Scripts/ShrinkPhase.cs
+++ UBR Tutorial Series/Assets/Scripts/ShrinkPhase.cs
@@ -10,31 +10,46 @@ namespace PolygonPilgrimage.BattleRoyaleKit
         /// The amount of seconds that must pass before the Zone Wall begins shrinking.
         /// </summary>
         [Tooltip("How many seconds will pass until the Zone Wall begins shrinking.")]
-        public int secondsUntilShrinkBegins;
+        [SerializeField]
+        private int secondsUntilShrinkBegins;
+
+        public int SecondsUntilShrinkBegins { get => secondsUntilShrinkBegins; } // readonly
 
         /// <summary>
         /// The amount of seconds it takes for the Zone Wall to completely shrink through this shrink phase.
         /// </summary>
         [Tooltip("How many seconds it takes for the Zone Wall to completely shrink through this shrink phase.")]
-        public int secondsToFullyShrink;
+        [SerializeField]
+        private int secondsToFullyShrink;
+
+        public int SecondsToFullyShrink { get => secondsToFullyShrink; } // readonly
 
         /// <summary>
         /// The radius at which the Zone Wall will stop shrinking this phase.
         /// </summary>
         [Tooltip("What radius should the Zone Wall stop shrinking at this phase?")]
-        public int shrinkToRadius;
+        [SerializeField]
+        private int shrinkToRadius;
+
+        public int ShrinkToRadius { get => shrinkToRadius; } // readonly
 
         /// <summary>
         /// The frequency of the damage ticks per second.
         /// </summary>
         [Tooltip("The frequency of the damage ticks per second.")]
-        public int ticksPerSecond;
+        [SerializeField]
+        private int ticksPerSecond;
+
+        public int TicksPerSecond { get => ticksPerSecond; } // readonly
 
         /// <summary>
         /// The amount of damage that is given every one time damage is dealt.
         /// </summary>
         [Tooltip("The amount of damage that is given every one time damage is dealt.")]
-        public float damagePerTick;
+        [SerializeField]
+        private float damagePerTick;
+
+        public float DamagePerTick { get => damagePerTick; } // readonly
 
         /// <summary>
         /// Creates a new ShrinkPhase with the given options.
@@ -44,7 +59,9 @@ namespace PolygonPilgrimage.BattleRoyaleKit
         /// <param name="shrinkToRadius">The radius at which the Zone Wall will stop shrinking this phase.</param>
         /// <param name="ticksPerSecond">The frequency of the damage ticks per second.</param>
         /// <param name="damagePerTick">The amount of damage that is given every one time damage is dealt.</param>
-        public ShrinkPhase(int secondsUntilShrinkBegins, int secondsToFullyShrink, int shrinkToRadius, int ticksPerSecond, float damagePerTick)
+        public ShrinkPhase(int secondsUntilShrinkBegins, 
+            int secondsToFullyShrink, int shrinkToRadius, 
+            int ticksPerSecond, float damagePerTick)
         {
             this.secondsUntilShrinkBegins = secondsUntilShrinkBegins;
             this.secondsToFullyShrink = secondsToFullyShrink;
