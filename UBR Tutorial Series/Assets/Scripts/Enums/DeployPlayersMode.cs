cd564ef555d3ea1d167633a59af9860ff83618ae
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-15 21:15:41 -0700

    new mode instead of bools

---
+++ UBR Tutorial Series/Assets/Scripts/Enums/DeployPlayersMode.cs
@@ -0,0 +1,13 @@
+﻿
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    /// <summary>
+    /// Mode for how the Players should start the game.
+    /// </summary>
+    public enum DeployPlayersMode
+    {
+        InPlane,
+        OnGround,
+        Skydiving,
+    }
+}
