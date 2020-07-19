bdac4e2e173ee1da87f9d8a22835650e534e0b3d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-19 15:38:46 -0700

    new folder for network scripts

---
+++ UBR Tutorial Series/Assets/Scripts/NetworkingScripts/ZoneWallNetwork.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Bolt;
+
+namespace PolygonPilgrimage.BattleRoyaleKit.Networking
+{
+    public class ZoneWallNetwork : EntityBehaviour<IZoneWallState>
+    {
+        private BRS_ZoneWallManager zoneWall;
+
+        public override void Attached()
+        {
+            // Awake();
+        }
+
+        public override void SimulateOwner()
+        {
+            //propagate scale
+        }
+    }
+
+}
