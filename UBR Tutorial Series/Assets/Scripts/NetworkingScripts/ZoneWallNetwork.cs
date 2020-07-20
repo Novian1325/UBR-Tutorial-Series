dc9b76c3266f0f22e14d2ce4f8525f6eeec31d95
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-19 20:42:27 -0700

    added networking logic

--- UBR Tutorial Series/Assets/Scripts/NetworkingScripts/ZoneWallNetwork.cs
+++ UBR Tutorial Series/Assets/Scripts/NetworkingScripts/ZoneWallNetwork.cs
@@ -7,14 +7,33 @@ namespace PolygonPilgrimage.BattleRoyaleKit.Networking
     {
         private BRS_ZoneWallManager zoneWall;
 
+        private void Awake()
+        {
+            zoneWall = GetComponent<BRS_ZoneWallManager>();
+        }
+
         public override void Attached()
         {
             // Awake();
+            state.SetTransforms(state.Transform, zoneWall.ZoneWallXform); // assign transform value
+
+            //set networking callbacks
+            state.AddCallback("Scale", MatchScale);
         }
 
         public override void SimulateOwner()
         {
+            //transform.position and .rotation are automatic
             //propagate scale
+            if (zoneWall.IsShrinking)
+            {
+                state.Scale = zoneWall.ZoneWallXform.localScale;
+            }
+        }
+
+        private void MatchScale()
+        {
+            zoneWall.ZoneWallXform.localScale = state.Scale;
         }
     }
 
