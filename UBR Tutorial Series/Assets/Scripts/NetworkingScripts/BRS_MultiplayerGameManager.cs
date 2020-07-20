e1a72bb8bd3d41d9039b873ba7a1cc3c3e9a3389
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-07-19 20:42:38 -0700

    testing bolt stuff

---
+++ UBR Tutorial Series/Assets/Scripts/NetworkingScripts/BRS_MultiplayerGameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Bolt;
+
+namespace PolygonPilgrimage.BattleRoyaleKit.Networking
+{
+    public class BRS_MultiplayerGameManager : GlobalEventListener
+    {
+        /// <summary>
+        /// Where the Players are when the game starts.
+        /// </summary>
+        [Header("GameSettings")]
+        [Tooltip("Where should the Players be when the game starts?")]
+        [SerializeField] private DeployPlayersMode deployPlayersMode = DeployPlayersMode.OnGround;
+
+        [Tooltip("Players in the game. If not a single player is found, a search will be done for all objects tagged \"Player\" in scene.")]
+        [SerializeField] private GameObject[] players;
+
+        [Header("SkyDiving")]
+        [Tooltip("This is the height the Player will start at if \"startSkyDiving\" is true.")]
+        [SerializeField] private int skyDiveTestHeight = 500;
+
+        /// <summary>
+        /// Bolt's version of Awake()
+        /// </summary>
+        public override void SceneLoadLocalDone(string sceneName,
+            IProtocolToken token)
+        {
+            Debug.Log("Attached", this);
+            //spawn game objects:
+            //spawn the Zone Wall
+            BoltNetwork.Instantiate(BoltPrefabs.UBR_Zone_Manager_Network, 
+                Vector3.zero, Quaternion.identity); // center of the world
+        }
+
+    }
+
+}
---
+++ UBR Tutorial Series/Assets/Scripts/NetworkingScripts/UBR_MasterServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using Bolt;
+using Bolt.Matchmaking;
+using UdpKit;
+
+namespace PolygonPilgrimage.BattleRoyaleKit.Networking
+{
+    public class UBR_MasterServer : GlobalEventListener
+    {
+        [SerializeField]
+        private string roomName = null;
+
+        private void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+            if(GUILayout.Button("Init Master Server", 
+                GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+            {
+                BoltLauncher.StartServer(); // Let it begin!
+            }
+
+            GUILayout.EndArea();
+        }
+
+        public override void BoltStartDone()
+        {
+            //Bolt has finished loading. Create a new session.
+            var matchName = !String.IsNullOrEmpty(roomName) ? roomName : // use name if it exists
+                Guid.NewGuid().ToString(); // otherwise generate a unique ID
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName, // set room name (can be anything!)
+                sceneToLoad: "NetworkTest"); // which scene to load
+        }
+
+        public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+        {
+            Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+            foreach (var session in sessionList)
+            {
+                var photonSession = session.Value as UdpSession;
+
+                if (photonSession.Source == UdpSessionSource.Photon)
+                {
+                    BoltMatchmaking.JoinSession(photonSession);
+                }
+            }
+        }
+    }
+}
